815f0cc5112945f072e8b4e5d5e7ae7d8f6e4716
a749311YdEnG <sixties-jingles-0w@icloud.com>
2025-10-04 21:55:57 +0900

    Implement 决策树生成器

---
+++ decision_tree_generator_1004_2055_jvr.py
@@ -0,0 +1,9 @@
+# 代码生成时间: 2025-10-04 20:55:56
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier, export_text
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from pyramid.view import view_config
+from pyramid.response import Response
+
+"
